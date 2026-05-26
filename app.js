0dcea033a13d613c0283b86c1b1fc66e38da7132
Ezhilarasi-E <admin.ezhil@healthmedpro.com>
2026-05-26 04:28:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello  Automation DevOps CI/CD Project');
+    res.send('Automation DevOps CI/CD Project');
 });
 
 app.get('/health', (req, res) => {
